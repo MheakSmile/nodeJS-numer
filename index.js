0c27bcc1d4cc78d60d2be623c1db3b9860d19973
Jirawat Puedjantuek <mheakzaza@hotmail.com>
2021-04-16 22:04:45 +0700

    swaggerAPI

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ import express from "express";
 import root_route from "./routes/root.route.js"
 import bodyParser from "body-parser"
 import cors from "cors"
-import linear_route from "./routes/Linear.route.js";
+import linear_route from "./routes/linear.route.js";
 import regression_route from "./routes/regression.route.js"
 import interpolation_route from "./routes/interpolation.route.js"
+
+import {createRequire} from 'module'
+const require = createRequire(import.meta.url)
+const swaggerDocument = require('./swagger.json')
+import swaggerUi from 'swagger-ui-express'
+
 const app = express();
 const PORT = 8080;
-
-
 
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument,{isExplorer: true})
+)
+
+
 app.use("/api/v1/root", root_route);
 app.use("/api/v1/linear", linear_route);
 app.use("/api/v1/regression",regression_route);
